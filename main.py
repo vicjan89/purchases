7451cd4b84cb2cb7517470f294d0e141fecd0fea
Yanush Viktar <vicjan89@gmail.com>
2021-10-23 11:42:45 +0300

    Данные содержатся только в дереве

--- main.py
+++ main.py
@@ -1,93 +1,83 @@
 import csv
 import os
 from tkinter import *
+import tkinter as tk
 from tkinter import filedialog as fd
+from tkinter import ttk
 
 root = Tk()
-scrollbar = Scrollbar(root)
-item_source = Listbox(yscrollcommand=scrollbar.set, width=80, selectmode=EXTENDED, exportselection=0)
-item_project = Listbox(yscrollcommand=scrollbar.set, width=80, selectmode=EXTENDED, exportselection=0)
-num = Listbox(yscrollcommand=scrollbar.set, width=7, exportselection=0)
-note = Listbox(yscrollcommand=scrollbar.set, width=30, exportselection=0)
-prefix_file_to_save=''
+root.title("Управление закупками")
+root.geometry("1300x700+0+0")
 
-def add(event):
-    ind=item_source.curselection()
-    for i in ind:
-        item_project.insert(END, item_source.get(i))
-        num.insert(END, 1)
-        note.insert(END,' ')
+# configure the grid layout
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
 
-def del_item_project(event):
-    ind=item_project.curselection()
-    for i in reversed(ind):
-        item_project.delete(i)
-        num.delete(i)
-        note.delete(i)
+# create a treeview
+tree = ttk.Treeview(root)
+tree.heading('#0', text='Закупки', anchor='w')
+
+FILENAME = r'C:\Users\Виктор\PycharmProjects\purchases\Спецификации.csv'
+date = []
+i1 = 0
+i2 = 0
+i3 = 0
+i = 0
+with open(FILENAME, "r", newline="", encoding='utf-8') as file1:
+    reader1 = csv.reader(file1)
+    for row1 in reader1:
+        tree.insert('', tk.END, text=row1[0], iid=i, open=True)
+        i1 = i
+        i += 1
+        with open(row1[0]+'.csv', 'r', newline="", encoding='utf-8') as file2:
+            reader2 = csv.reader(file2)
+            for row2 in reader2:
+                tree.insert(i1, tk.END, text=row2[0]+' - '+row2[1]+' шт.', iid=i, open=False)
+                i2 = i
+                i += 1
+                with open(row2[0]+'.csv', 'r', newline="", encoding='utf-8') as file3:
+                    reader3 = csv.reader(file3)
+                    for row3 in reader3:
+                        tree.insert(i2, tk.END, text=row3[0]+' \t '+row3[1]+' шт. \t '+row3[2], iid=i, open=False)
+                        i += 1
+
+
+# place the Treeview widget on the root window
+tree.grid(row=0, column=0, sticky='nsew')
+
+root.mainloop()
+
 
 def change_num_item_project(event):
-    ind=item_project.curselection()
-    d=0
+    ind = item_project.curselection()
+    d = 0
     if event.char == '+':
-        d=1
+        d = 1
     elif event.char == '-':
-        d=-1
+        d = -1
     elif event.char == '*':
-        d=10
+        d = 10
     elif event.char == '/':
         d = -10
     else:
         pass
     if ind == ():
         l = num.size()
-        ind = range(l-1,l)
+        ind = range(l - 1, l)
     for i in ind:
-        n=int(num.get(i))
-        if n+d>0:
+        n = int(num.get(i))
+        if n + d > 0:
             num.delete(i)
-            num.insert(i,n+d)
+            num.insert(i, n + d)
 
-def open_file(event):
-    file_name = fd.askopenfilename()
-    root.title(file_name)
-    item_project.delete(0,END)
-    num.delete(0,END)
-    note.delete(0,END)
-    with open(file_name, "r", newline="", encoding='utf-8') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            item_project.insert(END, row[0])
-            num.insert(END, row[1])
-            note.insert(END,row[2])
-
-def save_file(event):
-    global prefix_file_to_save
-    file_name = fd.asksaveasfilename(
-        filetypes=(("TXT files", "*.csv"),
-                   ("All files", "*.*")))
-    if file_name[-4:]=='.csv':
-        file_name=file_name[0:-4]
-    file_name=file_name+prefix_file_to_save+'.csv'
-    prefix_file_to_save = ''
-    root.title(file_name)
-    s = zip(item_project.get(0,END),num.get(0,END),note.get(0,END))
-    with open(file_name, "w", newline="", encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerows(s)
-
-def save_group(event):
-    global prefix_file_to_save
-    prefix_file_to_save='gp'
-    save_file(event)
 
 def project():
-
     root.title("Формирование спецификации проекта")
 
-    item_source.delete(0,END)
-    item_project.delete(0,END)
-    num.delete(0,END)
-    note.delete(0,END)
+    item_source.delete(0, END)
+    item_project.delete(0, END)
+    num.delete(0, END)
+    note.delete(0, END)
     FILENAME = r'C:\Users\Виктор\PycharmProjects\purchases\ТМЦ.csv'
     with open(FILENAME, "r", newline="", encoding='utf-8') as file:
         reader = csv.reader(file)
@@ -118,20 +108,14 @@ def project():
     note.pack(side=LEFT, fill=BOTH, expand=True)
     scrollbar.config(command=note.yview)
 
+
 def group_projects():
-
-    root.title("Формирование группы проектов")
-    item_source.delete(0,END)
-    item_project.delete(0,END)
-    num.delete(0,END)
-    note.delete(0,END)
-
     directory = r'C:\Users\Виктор\PycharmProjects\purchases'
     files = os.listdir(directory)
     for f in files:
-        if f[-4:]=='.csv' and f[-6:-4]!='gp' and f[0:3]!='ТМЦ':
-            f=f[0:-4]
-            item_source.insert(END,f)
+        if f[-4:] == '.csv' and f[-6:-4] != 'gp' and f[0:3] != 'ТМЦ':
+            f = f[0:-4]
+            item_source.insert(END, f)
 
     scrollbar.pack(side=RIGHT, fill=Y)
 
@@ -157,12 +141,13 @@ def group_projects():
     note.pack(side=LEFT, fill=BOTH, expand=True)
     scrollbar.config(command=note.yview)
 
+
 def view_sum(event):
     file_name = item_source.curselection()
-    progects=[]
-    items=[]
-    sum_items=[]
-    with open(item_source.get(file_name[0])+'gp.csv', "r", newline="", encoding='utf-8') as file:
+    progects = []
+    items = []
+    sum_items = []
+    with open(item_source.get(file_name[0]) + 'gp.csv', "r", newline="", encoding='utf-8') as file:
         reader = csv.reader(file)
         for row in reader:
             progects.append(row)
@@ -171,13 +156,13 @@ def view_sum(event):
         for row in reader:
             items.append(row[0])
     for prg in progects:
-        with open(prg[0]+'.csv', 'r', newline="", encoding='utf-8') as file:
-            reader=csv.reader(file)
+        with open(prg[0] + '.csv', 'r', newline="", encoding='utf-8') as file:
+            reader = csv.reader(file)
             for row in reader:
                 if row[0] in sum_items:
-                    i=sum_items.index(row[0])
+                    i = sum_items.index(row[0])
                     print(i)
-                    sum_items[i][1]+=1
+                    sum_items[i][1] += 1
                 else:
                     sum_items.append(row)
     for s in sum_items:
@@ -214,14 +199,3 @@ def sum_group_projects():
     scrollbar.config(command=num.yview)
     note.pack(side=LEFT, fill=BOTH, expand=True)
     scrollbar.config(command=note.yview)
-
-root.title("Управление закупками")
-root.geometry("1300x700+0+0")
-
-main_menu = Menu()
-main_menu.add_cascade(label="Проекты", command=project)
-main_menu.add_cascade(label="Группы проектов", command=group_projects)
-main_menu.add_cascade(label="Спецификация группы объектов", command=sum_group_projects)
-root.config(menu=main_menu)
-
-root.mainloop()
