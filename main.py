d1d0b10d5bbb6ea6da79fefb22fd773f7d0e65c9
Yanush Viktar <vicjan89@gmail.com>
2021-10-24 18:35:23 +0300

    Умеет редактировать элементы

--- main.py
+++ main.py
@@ -5,6 +5,26 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import ttk
 
+i = 0
+num_edit = 0
+
+def edit_item(event):
+    num_edit = int(tree.selection()[0])
+    text.set(data[num_edit][2])
+    num.set(data[num_edit][3])
+    note.set(data[num_edit][4])
+
+def set_item(event):
+    num_edit = int(tree.selection()[0])
+    data[num_edit][2] = text.get()
+    data[num_edit][3] = num.get()
+    data[num_edit][4] = note.get()
+    if data[0] == '':
+        prefix = ''
+    else:
+        prefix = ' шт.'
+    tree.item(num_edit, text = data[num_edit][2]+'\t'+data[num_edit][3]+prefix+'\t'+data[num_edit][4])
+
 root = Tk()
 root.title("Управление закупками")
 root.geometry("1300x700+0+0")
@@ -15,60 +35,56 @@ root.columnconfigure(0, weight=1)
 
 # create a treeview
 tree = ttk.Treeview(root)
-tree.heading('#0', text='Закупки', anchor='w')
+tree.heading('#0', text='Группы проектов', anchor='w')
 
 FILENAME = r'C:\Users\Виктор\PycharmProjects\purchases\Спецификации.csv'
-date = []
+data = []
 i1 = 0
 i2 = 0
 i3 = 0
-i = 0
+
 with open(FILENAME, "r", newline="", encoding='utf-8') as file1:
     reader1 = csv.reader(file1)
     for row1 in reader1:
-        tree.insert('', tk.END, text=row1[0], iid=i, open=True)
+        data.append(['', i, row1[0], row1[1], row1[2]])
         i1 = i
         i += 1
         with open(row1[0]+'.csv', 'r', newline="", encoding='utf-8') as file2:
             reader2 = csv.reader(file2)
             for row2 in reader2:
-                tree.insert(i1, tk.END, text=row2[0]+' - '+row2[1]+' шт.', iid=i, open=False)
+                data.append([i1,i, row2[0],row2[1], row2[2]])
                 i2 = i
                 i += 1
                 with open(row2[0]+'.csv', 'r', newline="", encoding='utf-8') as file3:
                     reader3 = csv.reader(file3)
                     for row3 in reader3:
-                        tree.insert(i2, tk.END, text=row3[0]+' \t '+row3[1]+' шт. \t '+row3[2], iid=i, open=False)
+                        data.append([i2, i, row3[0], row3[1], row3[2]])
                         i += 1
+for item in data:
+    if item[0] == '':
+        prefix = ''
+    else:
+        prefix = ' шт.'
+    tree.insert(item[0], item[1],iid=item[1],text=item[2]+'\t'+item[3]+prefix+'\t'+item[4])
 
-
-# place the Treeview widget on the root window
-tree.grid(row=0, column=0, sticky='nsew')
-
+tree.bind("<<TreeviewSelect>>", edit_item)
+text=StringVar()
+edit_text = Entry(textvariable=text)
+edit_text.place(relx=0, y=1, relwidth=0.5)
+num=StringVar()
+edit_num = Spinbox(textvariable=num, from_ = 1.0 , to = 100000.0, increment = 1.0)
+edit_num.place(relx=0.5, y=1, relwidth=0.1)
+note=StringVar()
+edit_note = Entry(textvariable=note)
+edit_note.place(relx=0.6, y=1, relwidth=0.4)
+tree.place(x=0, y=22, relheight=1, relwidth=1)
+edit_text.bind('<Return>',set_item)
+edit_num.bind('<Return>',set_item)
+edit_note.bind('<Return>',set_item)
 root.mainloop()
 
 
-def change_num_item_project(event):
-    ind = item_project.curselection()
-    d = 0
-    if event.char == '+':
-        d = 1
-    elif event.char == '-':
-        d = -1
-    elif event.char == '*':
-        d = 10
-    elif event.char == '/':
-        d = -10
-    else:
-        pass
-    if ind == ():
-        l = num.size()
-        ind = range(l - 1, l)
-    for i in ind:
-        n = int(num.get(i))
-        if n + d > 0:
-            num.delete(i)
-            num.insert(i, n + d)
+
 
 
 def project():
